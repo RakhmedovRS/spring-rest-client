ac426b98ea80dd2129470b2354c15758a12f91bd
rakhmedovrs <rakhmedovrs@gmail.com>
2020-07-28 21:36:46 +0300

    Initial commit

---
+++ src/test/java/com/github/rakhmedovrs/springrestclient/SpringRestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rakhmedovrs.springrestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
